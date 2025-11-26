cfdf62601581873882a4a497b97417fb990940ce
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-26 10:52:14 +0700

    tambah file config.js,db.js,index.js,komik.js dan perbaikan

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectionDatabase = require('./config/db');
+//const apiRoute = require('./routes/api');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', apiRoute);
+
+async function startServer() {
+  await connectionDatabase();
+    app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();  
