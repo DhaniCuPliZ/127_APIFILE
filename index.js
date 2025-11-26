a1f32d4f1f7fa991dfcc77c500e62c8bf8d661c2
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-26 11:18:48 +0700

    Menambahkan endpoint komik (create, getAll, getById, update, delete)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const connectionDatabase = require('./config/db');
-//const apiRoute = require('./routes/api');
+const apiRoute = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', apiRoute);
+app.use('/api', apiRoute);
 
 async function startServer() {
   await connectionDatabase();
